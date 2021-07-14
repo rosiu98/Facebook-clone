ee1f876d3c31aa2e276053298245d26ee4b1abea
Paweł <rosiu978@gmail.com>
2021-07-14 13:10:02 +0200

    adding input box and connection db in firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase";
+import "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBR-uC1Ks8CynQNp17fbjuNCbGGZcm8mH8",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "1015570168334",
   appId: "1:1015570168334:web:26c8470e059f5a682ada5f"
 };
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+const storage = firebase.storage();
+
+export { db, storage};
